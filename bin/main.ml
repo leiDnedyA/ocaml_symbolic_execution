82d40e851fd658f1aeb0169c6f0350c50205c52f
leiDnedyA <aydendiel@gmail.com>
2024-01-13 21:59:05 -0500

    feat: implemented and informally tested eval_bool function using z3

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+open Lib;;
+
+let result = eval_bool (Not (Value (Bool true)));;
+
+let extract_bool v =
+  match v with
+  | Bool b -> b
+  | _ -> failwith("Not a Bool Value")
+;;
+
+Printf.printf "%b\n" (extract_bool result)
--- lib/lib.ml
+++ lib/lib.ml
@@ -1,4 +1,5 @@
 open Z3;;
+(* open Types;; *)
 
 type value =
   | Bool of bool
@@ -24,26 +25,32 @@ type expr =
   | Or of expr * expr
 ;;
 
-(* type alg_expr = *)
-(*   (* Algebra *) *)
-(*   | Addition of expr * expr *)
-(*   | Subtraction of expr * expr *)
-(*   | Multiplication of expr * expr *)
-(*   | Division of expr * expr *)
-(* ;; *)
-(**)
-(* type ineq_expr = *)
-(*   (* Inequalities *) *)
-(*   | Eq of expr * expr *)
-(*   | Gt of expr * expr *)
-(*   | Gte of expr * expr *)
-(*   | Lt of expr * expr *)
-(*   | Lte of expr * expr *)
-(* ;; *)
-(**)
-(* type bool_expr = *)
-(*   (* Boolean *) *)
-(*   | Not of expr *)
-(*   | And of expr * expr *)
-(*   | Or of expr * expr *)
-(* ;; *)
+(* Boolean solving *)
+let bool_of_z3 e = Z3.Boolean.is_true e;;
+
+let solve_z3_bool_expr expr mk =
+  let solver = (Solver.mk_simple_solver mk) in
+    let model = Option.get (Solver.get_model solver) in
+    bool_of_z3 (Option.get (Model.eval model expr true));;
+
+let rec z3_expr_of_expr expr mk =
+  match expr with
+  | Value v -> (match v with
+    | Bool b -> Z3.Boolean.mk_val mk b
+    | _ -> failwith("invalid boolean expr"))
+  | And (x, y) -> Z3.Boolean.mk_and mk [
+    (z3_expr_of_expr x mk);
+    (z3_expr_of_expr y mk)]
+  | Or (x, y) -> Z3.Boolean.mk_or mk [
+    (z3_expr_of_expr x mk);
+    (z3_expr_of_expr y mk)]
+  | Not x -> Z3.Boolean.mk_not mk (z3_expr_of_expr x mk)
+  | _ -> failwith("invalid boolean expression");;
+
+
+let eval_bool expr =
+  let mk = mk_context [] in
+    let bool_result = solve_z3_bool_expr (z3_expr_of_expr expr mk) mk in
+      if bool_result == true then (Bool true) else (Bool false);;
+
+let result = eval_bool (Not (Value (Bool false)))
